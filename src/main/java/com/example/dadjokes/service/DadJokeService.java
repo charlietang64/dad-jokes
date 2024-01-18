79ff14d9a2fbdb6d4d2de20f551e0f449bafc658
Charlie Tang <tang.charlie@student.greenriver.edu>
2024-01-18 14:40:58 -0800

    Created service layer with getAll, add, update, delete

---
+++ src/main/java/com/example/dadjokes/service/DadJokeService.java
@@ -0,0 +1,31 @@
+package com.example.dadjokes.service;
+
+import com.example.dadjokes.db.DadJokeRepository;
+import com.example.dadjokes.domain.DadJoke;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DadJokeService {
+    private DadJokeRepository repository;
+    public DadJokeService(DadJokeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<DadJoke> getAll() {
+        List<DadJoke> jokes = repository.findAll();
+        return Collections.unmodifiableList(jokes);
+    }
+
+    public void add(DadJoke joke) {
+        repository.save(joke);
+    }
+
+    public void update(int id, String newJokeText) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+}
